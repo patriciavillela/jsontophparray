c19617d458a3f3aecea1184debcdb3768f5ae85f
Patrícia Villela Ocaña Bruno <patriciavillela@protonmail.com>
2022-11-07 10:08:17 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# jsontophparray README
+
+## Features
+
+Formats JSON to PHP Array.
+
+![usage](./animations/usage.gif)
+
+> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+
+## Release Notes
+
+### 1.0.0beta
+
+Initial release of the JSON to PHP Array converter.
---
+++ extension.js
@@ -0,0 +1,84 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+const editor = vscode.window.activeTextEditor;
+const writer = vscode.window.writer;
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "jsontophparray" is now active!');
+
+	let format = vscode.commands.registerCommand('jsontophparray.jsonToPHPArray', function() {
+		vscode.window.showInformationMessage('Salve galera!');
+		const selectedText = editor.document.getText(editor.selection);
+		try {
+			const jsonObject = JSON.parse(selectedText);
+			const converted = formatJSONtoPHPArray(jsonObject);
+			editor.edit(editBuilder => {
+				editBuilder.replace(
+					new vscode.Range(
+						editor.selection.start.line,
+						editor.selection.start.character,
+						editor.selection.end.line,
+						editor.selection.end.character
+					),
+					converted.replaceAll("\n\n", "\n")
+				);
+			});
+		} catch(exception) {
+			console.log(exception);
+			vscode.window.showInformationMessage('Invalid JSON');
+		}
+	});
+
+	context.subscriptions.push(format);
+}
+
+function formatJSONtoPHPArray(obj, level = 1) {
+	let PHPArray = "";
+	const keys = Object.keys(obj);
+	keys.forEach((key, index) => {
+		let string;
+		if(obj[key].constructor.name === "Object") {
+			string = formatJSONtoPHPArray(obj[key], level + 1);
+		} else if(obj[key].constructor.name === "Array") {
+			PHPArray += indent(level) + "'" + key + "' => " + JSON.stringify(obj[key]);
+			if(keys[index + 1]) {
+				PHPArray += ",";
+			}
+			return;
+		} else if(obj[key].constructor.name === "Number") {
+			string = obj[key];
+		} else if(obj[key].constructor.name === "String") {
+			string = "'" + obj[key] + "'";
+		} else {
+			throw new Error('Invalid JSON');
+		}
+		PHPArray += indent(level) + "'" + key + "' => " + string;
+		if(keys[index + 1]) {
+			PHPArray += ",";
+		}
+		PHPArray += "\n";
+	});
+	return "[\n" + PHPArray + "\n" + indent(level - 1) + "]";
+}
+
+function indent(level) {
+	return " ".repeat(level * 4);
+}
+
+// This method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
